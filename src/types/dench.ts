5f337e7336d853742a5ca503453dd6b93f0e4dc1
Jung TaeSeung <mike415415@naver.com>
2026-06-03 01:14:19 +0900

    refactor : dench builder 타입 정리 1

--- src/types/dench.ts
+++ src/types/dench.ts
@@ -25,71 +25,117 @@ export interface DenchConfig extends DenchBaseConfig{
     timeout? : number;
 }
 
-export interface DenchBuilder<T>{
+export interface DenchBuilder<T, R extends DenchBuilder<T, R>>{
     config : DenchBaseConfig,
+    /**
+     * 인증 토큰 설정을 추가합니다.
+     * @config { Authorization : `Bearer ${token}`}
+     */
+    auth: (token: string) => R,
+
+
+    /**
+     * credentials 설정을 추가합니다.
+     * @config { credentials : credentials }
+     */
+    credentials: (credentials: HTTPCredentials) => R,
+
+    /**
+     * mode 설정을 추가합니다.
+     * @config { mode : mode }
+     */
+    mode: (mode: HTTPMode) => R,
+
+    /**
+     * fetch의 cache 설정을 추가합니다.
+     * @config { cache : cache }
+     */
+    cache: (cache: HTTPCache) => R,
+
+    /**
+     * fetch의 redirect 설정을 추가합니다.
+     * @config { redirect : redirect }
+     */
+    redirect: (redirect: HTTPRedirect) => R,
+
+    /**
+     * referrerPolicy 설정을 추가합니다.
+     * @config { referrerPolicy : policy }
+     */
+    referrerPolicy: (policy: HTTPReferrerPolicy) => R,
+    
+
+    /**
+     * 요청을 중단할 수 있는 AbortController를 설정합니다.
+     * @param controller AbortController 인스턴스
+     * @returns DenchGetBuilder<T>
+     */
+    abort: (controller: AbortController) => R,
+
+    /**
+     * 요청 timeout 설정을 추가합니다.
+     * @param ms ms 단위 timeout 시간
+     */
+    timeout: (ms: number) => R,
+
+
+    /**
+     * 에러 콜백을 설정합니다. 
+     * fetch 과정에서 에러가 발생 시 해당 콜백이 호출됩니다.
+     */
+    error: (callback: (error: unknown) => void) => R,
+
+
+
+    /**
+     * URL의 슬래쉬 경계 부분을 정규화 합니다.
+     * 
+     * 1.  baseURL 끝의 슬래쉬를 제거하고 apiURL의 시작엔 최소 한개의 슬래쉬를 생성한다.
+     * 2.  baseURL과 apiURL에 슬래쉬가 중복 발생하는 모든 경우에 하나로 바꾼다.
+     * 
+     * @example1 http://example.com/ -> http://example.com
+     * @example2 http://example.com + //api// -> http://example.com + /api
+     * @example3 https:////example.com + //api/aa -> https://example.com + /api/aa
+     * 
+     * 만약 슬래시를 두 개 이상 사용하는 것이 의도라면 해당 함수를 사용하지 마세요
+     * 
+     * @returns 
+     */
+    boundaryNormalize: () => R,
+
+
+    /**
+    * URL을 더 엄격하게 정규화 합니다.
+    * 
+    * 1.  baseURL 끝의 슬래쉬를 제거하고 apiURL의 시작엔 최소 한개의 슬래쉬를 생성한다.
+    * 2.  baseURL과 apiURL에 슬래쉬가 중복 발생하는 모든 경우에 하나로 바꾼다.
+    * 3.  apiURL의 끝 부분 슬래쉬를 제거한다.
+    * 4.  http: 또는 https: 에는 슬래쉬가 정확히 두 개 오게 한다. 
+    * 
+    * @example1 http://example.com/ -> http://example.com
+    * @example2 http://example.com + //api// -> http://example.com + /api
+    * @example3 https:////example.com + //api/aa -> https://example.com + /api/aa
+    * 
+    * tip : 만약 슬래시를 두 개 이상 사용하는 것이 의도라면 해당 함수를 사용하지 마세요
+    * 또한 성능적인 소모가 존재하니 예측 불가능한 URL이 들어오는 경우에 사용하고, 
+    * URL을 직접 눈으로 확인해서 관리하는 것을 권장합니다.
+    * 
+    * @returns 
+    */
+    hardNormalize: () => R
+
 }
+
+
+
+
 
 /**
  * Dench post, put 타입 요청의 빌더 인터페이스
  * 
  * @interface DenchCreateBuilder
  */
-export interface DenchCreateBuilder<T> extends DenchBuilder<T>, DenchRunner<T>{
-    /**
-     * abortController를 사용자 요청을 취소합니다
-     * @param controller 
-     * @returns 
-     */
-    abort: (controller: AbortController) => DenchCreateBuilder<T>,
-    
-    /**
-     * auth token 설정을 추가합니다.
-     * @config { Authorization : `Bearer ${token}`}
-     */
-    auth: (token:string) => DenchCreateBuilder<T>,
-
-    /**
-     * credentials 설정을 추가합니다.
-     * @config { credentials : credentials }
-     */
-    credentials: (credentials: HTTPCredentials) => DenchCreateBuilder<T>,
-
-    /**
-     * timeout 설정을 추가합니다.
-     * @param ms ms 단위 timeout 시간
-     */
-    timeout: (ms : number) => DenchCreateBuilder<T>,
-
-    /**
-     * mode 설정을 추가합니다.
-     * @config { mode : mode }
-     */
-    mode : (mode : HTTPMode) => DenchCreateBuilder<T>,
-
-    /**
-     * fetch의 cache 설정을 추가합니다.
-     * @config { cache : cache }
-     */
-    cache : (cache : HTTPCache) => DenchCreateBuilder<T>,
-
-    /**
-     * fetch의 redirect 설정을 추가합니다.
-     * @config { redirect : redirect }
-     */
-    redirect : (redirect : HTTPRedirect) => DenchCreateBuilder<T>,
-
-    /**
-     * referrerPolicy 설정을 추가합니다.
-     * @config { referrerPolicy : policy }
-     */
-    referrerPolicy : (policy : HTTPReferrerPolicy) => DenchCreateBuilder<T>,
-
-    /**
-     * 에러 콜백을 설정합니다. 
-     * fetch 과정에서 에러가 발생 시 해당 콜백이 호출됩니다.
-     */
-    error: (callback: (error: unknown) => void) => DenchCreateBuilder<T>,
-
+export interface DenchCreateBuilder<T> extends DenchBuilder<T, DenchCreateBuilder<T>>, DenchRunner<T>{
     /**
      * 요청 데이터를 JSON 형식으로 전송합니다.
      * @header { "Content-Type" : "application/json" }
@@ -105,43 +151,6 @@ export interface DenchCreateBuilder<T> extends DenchBuilder<T>, DenchRunner<T>{
      * @header { "Content-Type" : "application/octet-stream" }
      */
     sendBlob : () => DenchCreateBuilder<T>,
-
-    /**
-     * URL의 슬래쉬 경계 부분을 정규화 합니다.
-     * 
-     * 1.  baseURL 끝의 슬래쉬를 제거하고 apiURL의 시작엔 최소 한개의 슬래쉬를 생성한다.
-     * 2.  baseURL과 apiURL에 슬래쉬가 중복 발생하는 모든 경우에 하나로 바꾼다.
-     * 
-     * @example1 http://example.com/ -> http://example.com
-     * @example2 http://example.com + //api// -> http://example.com + /api
-     * @example3 https:////example.com + //api/aa -> https://example.com + /api/aa
-     * 
-     * 만약 슬래시를 두 개 이상 사용하는 것이 의도라면 해당 함수를 사용하지 마세요
-     * 
-     * @returns 
-     */
-    boundaryNormalize : () => DenchCreateBuilder<T>,
-
-
-    /**
-        * URL을 더 엄격하게 정규화 합니다.
-        * 
-        * 1.  baseURL 끝의 슬래쉬를 제거하고 apiURL의 시작엔 최소 한개의 슬래쉬를 생성한다.
-        * 2.  baseURL과 apiURL에 슬래쉬가 중복 발생하는 모든 경우에 하나로 바꾼다.
-        * 3.  apiURL의 끝 부분 슬래쉬를 제거한다.
-        * 4.  http: 또는 https: 에는 슬래쉬가 정확히 두 개 오게 한다. 
-        * 
-        * @example1 http://example.com/ -> http://example.com
-        * @example2 http://example.com + //api// -> http://example.com + /api
-        * @example3 https:////example.com + //api/aa -> https://example.com + /api/aa
-        * 
-        * tip : 만약 슬래시를 두 개 이상 사용하는 것이 의도라면 해당 함수를 사용하지 마세요
-        * 또한 성능적인 소모가 존재하니 예측 불가능한 URL이 들어오는 경우에 사용하고, 
-        * URL을 직접 눈으로 확인해서 관리하는 것을 권장합니다.
-        * 
-        * @returns 
-        */
-    hardNormalize : () => DenchCreateBuilder<T>
 }
 
 
@@ -150,96 +159,9 @@ export interface DenchCreateBuilder<T> extends DenchBuilder<T>, DenchRunner<T>{
  * 
  * @interface DenchGetBuilder
  */
-export interface DenchGetBuilder<T> extends DenchBuilder<T>, DenchRunner<T>{
-    /**
-     * 요청을 중단할 수 있는 AbortController를 설정합니다.
-     * @param controller AbortController 인스턴스
-     * @returns DenchGetBuilder<T>
-     */
-    abort: (controller : AbortController) => DenchGetBuilder<T>,
+export interface DenchGetBuilder<T> extends DenchBuilder<T, DenchGetBuilder<T>>, DenchRunner<T>{
 
-    /**
-     * 인증 토큰 설정을 추가합니다.
-     * @config { Authorization : `Bearer ${token}`}
-     */
-    auth: (token:string) => DenchGetBuilder<T>,
 
-    /**
-     * 요청 timeout 설정을 추가합니다.
-     * @param ms ms 단위 timeout 시간
-     */
-    timeout : (ms : number) => DenchGetBuilder<T>,
-
-    /**
-     * credentials 설정을 추가합니다.
-     * @config { credentials : credentials }
-     */
-    credentials: (credentials : HTTPCredentials) => DenchGetBuilder<T>,
-
-    /**
-     * fetch의 cache 설정을 추가합니다.
-     * @config { cache : cache }
-     */
-    cache : (cache : HTTPCache) => DenchGetBuilder<T>,
-
-    /**
-     * mode 설정을 추가합니다.
-     * @config { mode : mode }
-     */
-    mode : (mode : HTTPMode) => DenchGetBuilder<T>,
-
-    /**
-     * redirect 설정을 추가합니다.
-     * @config { redirect : redirect }
-     */
-    redirect : (redirect : HTTPRedirect) => DenchGetBuilder<T>,
-
-    /**
-     * referrerPolicy 설정을 추가합니다.
-     * @config { referrerPolicy : policy }
-     */
-    referrerPolicy: (policy: HTTPReferrerPolicy) => DenchGetBuilder<T>,
-
-    /**
-     * 에러 콜백을 설정합니다. 
-     * fetch 과정에서 에러가 발생 시 해당 콜백이 호출됩니다.
-     */
-    error: (callback: (error: unknown) => void) => DenchGetBuilder<T>,
-    
-    /**
-     * URL의 슬래쉬 경계 부분을 정규화 합니다.
-     * 
-     * 1.  baseURL 끝의 슬래쉬를 제거하고 apiURL의 시작엔 최소 한개의 슬래쉬를 생성한다.
-     * 2.  baseURL과 apiURL에 슬래쉬가 중복 발생하는 모든 경우에 하나로 바꾼다.
-     * 
-     * @example1 http://example.com/ -> http://example.com
-     * @example2 http://example.com + //api// -> http://example.com + /api
-     * @example3 https:////example.com + //api/aa -> https://example.com + /api/aa
-     * 
-     * 만약 슬래시를 두 개 이상 사용하는 것이 의도라면 해당 함수를 사용하지 마세요
-     * 
-     * @returns 
-     */
-    boundaryNormalize: () => DenchGetBuilder<T>,
-
-    /**
-     * URL을 더 엄격하게 정규화 합니다.
-     * 
-     * 1.  baseURL 끝의 슬래쉬를 제거하고 apiURL의 시작엔 최소 한개의 슬래쉬를 생성한다.
-     * 2.  baseURL과 apiURL에 슬래쉬가 중복 발생하는 모든 경우에 하나로 바꾼다.
-     * 3.  apiURL의 끝 부분 슬래쉬를 제거한다.
-     * 4.  http: 또는 https: 에는 슬래쉬가 정확히 두 개 오게 한다. 
-     * 
-     * @example1 http://example.com/ -> http://example.com
-     * @example2 http://example.com + //api// -> http://example.com + /api
-     * @example3 https:////example.com + //api/aa -> https://example.com + /api/aa
-     * 
-     * 만약 슬래시를 두 개 이상 사용하는 것이 의도라면 해당 함수를 사용하지 마세요
-     * 또한 성능적인 소모가 존재하니 예측 불가능한 URL이 들어오는 경우에만 사용하세요
-     * 
-     * @returns 
-     */
-    hardNormalize: () => DenchGetBuilder<T>
 }
 
 
@@ -259,6 +181,8 @@ export interface DenchInterface{
     put : <T>(api: string, data : any) => DenchCreateBuilder<T>
     delete : <T>(api: string) => DenchGetBuilder<T>
 }
+
+
 
 /**
  * Dench 요청 실행 인터페이스
