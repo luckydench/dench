91857632997dc2c26f9d0f59b9bbd16758d332e0
Jung TaeSeung <mike415415@naver.com>
2026-06-09 18:34:12 +0900

    feat : DenchURLNormalizMode export 추가

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { dench } from './denchfetch/dench';
 export { default as denchfetcher } from './denchfetch/denchfetcher';
 export { HTTPCredentials, HTTPMode, HTTPReferrerPolicy, HTTPCache, HTTPRedirect } from './types/denchHTTPEnum';
-export { DenchAuthType } from './types/denchEnum';
+export { DenchAuthType, DenchURLNormalizeMode } from './types/denchEnum';
 export type {  DenchRunner }  from './types/denchRunner';
 export type { DenchBuilder, DenchCreateBuilder, DenchGetBuilder } from './types/denchBuilder';
 export type { DenchConfig } from './types/denchConfig';
